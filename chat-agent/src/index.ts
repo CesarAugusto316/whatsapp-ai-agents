0f90f469325411967a61238748aef7c074def577
cesaraugusto316 <cesaraugusto316@protonmail.com>
2025-12-10 14:39:14 -0500

    ✨ cloudflare ai agents

--- chat-agent/src/index.ts
+++ chat-agent/src/index.ts
@@ -1,24 +1,27 @@
 import { Hono } from "hono";
-import { env } from "cloudflare:workers";
 import { WahaRecievedEvent } from "@/types/received-event";
+import { MyAgent } from "@/services/ai.service";
+import { env } from "cloudflare:workers";
 import whatsappService from "@/services/whatsapp.service";
+import { cors } from "hono/cors";
+import { getAgentByName } from "agents";
 
+// AI SDK PROJECT EXAMPLE
+// https://github.com/gopinav/Next.js-AI-Tutorials/tree/main/src/app/api
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-// app.get("/hello", async (c) => {
-//   await whatsappService.sendText({
-//     // chatId: "593984393446@c.us",
-//     chatId: "593 98 439 3446@c.us",
-//     text: "Waha API",
-//     session: "default",
-//   });
-//   return c.json({ received: true, message: "AI Agent Response" });
-// });
+app.use(
+  cors({
+    origin: ["*"],
+    allowHeaders: ["Content-Type"],
+    allowMethods: ["POST", "OPTIONS"],
+  }),
+);
 
 app.post("/received-messages/:businessId", async (c) => {
   // if (!verifySignature(c.req.raw)) {
   //   return c.json({ error: "Invalid signature" }, 401);
-  const id = c.req.param("businessId");
+  const businessId = c.req.param("businessId");
   const msgResponse = await c.req.json<WahaRecievedEvent>();
 
   if (msgResponse.event !== "message") {
@@ -37,8 +40,12 @@ app.post("/received-messages/:businessId", async (c) => {
     },
     async () =>
       (
-        await c.env.AI.run("@cf/meta/llama-3.1-8b-instruct-fp8", {
+        await c.env.AI.run("@cf/ibm-granite/granite-4.0-h-micro", {
           prompt: msgResponse.payload.body,
+          messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: msgResponse.payload.body },
+          ],
         })
       ).response?.toString() || "AI response",
   );
@@ -48,10 +55,20 @@ app.post("/received-messages/:businessId", async (c) => {
     text: aiResponse,
     session: msgResponse.session,
   });
-  return c.json({ received: true, message: "AI Agent Response" });
+
+  // Named addressing
+  // Best for: convenience method for creating or retrieving an agent by name/ID.
+  const agent = await getAgentByName<CloudflareBindings, MyAgent>(
+    c.env.MyAgent,
+    businessId,
+  );
+  // Pass the request to our Agent instance
+  return await agent.fetch(c.req.raw);
+  // return c.json({ received: true, message: "AI Agent Response" });
 });
 
 export default app;
+export { MyAgent };
 
 // Verify webhook signature to ensure it's from WasenderApi
 function verifySignature(req: Request) {
