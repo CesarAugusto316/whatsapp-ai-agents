ed47f157af415202a76cd0983cc57d32d29828d7
cesaraugusto316 <cesaraugusto316@protonmail.com>
2026-02-10 17:26:58 -0500

    ✅ feat: Remove unused config from generateText

--- chat-agent/src/infraestructure/adapters/ai/ai.adapter.interface.ts
+++ chat-agent/src/infraestructure/adapters/ai/ai.adapter.interface.ts
@@ -2,10 +2,7 @@ import { EmbeddingItem, EmbeddingRequest } from "./embeddings.types";
 import { MessagesBasedRequest } from "./open-ai-compatible.types";
 
 export interface IAiAdapter {
-  generateText(
-    args: MessagesBasedRequest,
-    config: { prompt: string; useAux: boolean },
-  ): Promise<string>;
+  generateText(args: MessagesBasedRequest): Promise<string>;
 
   /**
    * @link https://platform.openai.com/docs/api-reference/embeddings
